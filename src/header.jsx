14bef22963e123a158d9cb68f2ad8aa3551904d4
Louis Hu <838511743hj@gmail.com>
2025-06-24 17:25:08 +0800

    system init

---
+++ src/header.jsx
@@ -0,0 +1,12 @@
+function Header() {
+  return (
+    <header className="bg-gray-800 text-white p-4">
+      <h1 className="text-2xl font-bold">My Job Apply</h1>
+      <nav className="mt-2">
+
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
